850c510f6fc5abfd15c57f344324675d0d000c25
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2024-06-10 09:55:35 +0200

    Add files via upload

--- scrittura_cinese/php/generatore.php
+++ scrittura_cinese/php/generatore.php
@@ -13,23 +13,23 @@
             //echo"<br>Connessione al server riuscita e selezione database riuscita";
 
             
-        $libro = $_POST["testo"];
+        $inizio = $_POST["inizio"];
+		$fine = $_POST["fine"];
         $numero_lettere = $_POST["numero_lettere"];
+		echo"$inizio $fine";
         $query_sql="SELECT* FROM lettere";
 	    $risultato_query=$connessione->query($query_sql);
 	    $righe=$risultato_query->num_rows;
         echo"Numero di lettere presenti del database: $righe <br>";
 		echo"<button type='button' onclick='return ricarica_pagina()'>Ricarica pagina</button>";
+		echo"<a href='../index.html'>Pagina home</a>";
 			echo"<table border=1>";
 			echo"<tr><td>ID</td><td>Lettera</td><td>Pinyin</td><td>Traduzione</td><td>Libro</td></tr>";
 			for($i=0;$i<$numero_lettere;$i++){
-				$numero_casuale = rand(1, $righe);
-				// Stampa il numero casuale generato
-            	//echo "Il numero casuale generato è: $numero_casuale";
-				$sql = "SELECT * FROM lettere WHERE id = $numero_casuale";
-				$risultato_query=$connessione->query($sql);
-				
-				while(($riga=$risultato_query->fetch_array())==TRUE){
+				$numero_casuale = rand($inizio,$fine);
+				$sql1 = "SELECT * FROM lettere WHERE id = $numero_casuale";
+				$risultato_query1=$connessione->query($sql1);
+				while(($riga=$risultato_query1->fetch_array())==TRUE){
 								echo"<tr><td>".$riga["ID"]."</td>";
 								echo"<td>".$riga["Lettera"]."</td>";
 								echo"<td>".$riga["Pinyin"]."</td>";
